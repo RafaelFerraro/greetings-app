83f6501e76bde4b14e5b6a42a84c65f2bf7b6e3c
Rafael Ferraro <ferraromv@gmail.com>
2021-11-24 12:04:55 -0300

    Adding filters for greetings endpoint

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
 require 'sinatra'
 require 'sequel'
 
+DB = Sequel.connect(ENV['DATABASE_URL'])
+
 get '/greetings' do
-  'Hello!'
+  greetings = DB[:greetings]
+
+  return greetings.all.to_json unless params[:country]
+
+  greeting = greetings.where(:country => params[:country]).first
+  greeting ? greeting.to_json : "There isn't any greeting for this country"
 end
 
 post '/greetings' do
   data = JSON.parse(request.body.read)
 
-  DB = Sequel.connect(ENV['DATABASE_URL'])
   greetings = DB[:greetings]
   greetings.insert(:country => data["country"], :greeting => data["greeting"])
 
